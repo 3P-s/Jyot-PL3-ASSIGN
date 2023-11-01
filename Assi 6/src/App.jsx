3f2bdfd847e8b8b168697f80c7ce2b579854540b
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-11-01 10:06:03 +0530

    Renamed

---
+++ Assi 6/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Counter from './Pages/Counter'
+import Home from './Pages/Home'
+
+function App() {
+
+  return (
+    <div>
+      <Home/>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
