3eaf2a3c5c84a2dd8cebfe3c3f009cc8b92bff1b
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-11-21 13:26:03 +0530

    changes

--- Assi 7/server.js
+++ Assi 7/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
+app.use(cors());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 const gamesRouter = require('./routes/games');
 app.use(express.json());
@@ -9,4 +11,4 @@ app.use('/api/games', gamesRouter);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+}); 
