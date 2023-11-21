fb4c05b07f37c887abe2450943457247e4e7a324
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-11-21 10:03:55 +0530

    Upadation

--- Assi 7/routes/games.js
+++ Assi 7/routes/games.js
@@ -15,18 +15,17 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/', (req, res) => {
-  Game.find({}, (err, games) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(games);
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    const games = await Game.find({});
+    res.json(games);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-// Retrieve a game by ID
+
 router.get('/:id', (req, res) => {
   Game.findById(req.params.id, (err, game) => {
     if (err) {
@@ -42,7 +41,7 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// Update a game by ID
+
 router.put('/:id', (req, res) => {
   Game.findByIdAndUpdate(
     req.params.id,
@@ -63,20 +62,19 @@ router.put('/:id', (req, res) => {
   );
 });
 
-// Delete a game by ID
-router.delete('/:id', (req, res) => {
-  Game.findByIdAndRemove(req.params.id, (err, game) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const game = await Game.findByIdAndDelete(req.params.id);
+    if (!game) {
+      res.status(404).json({ error: 'Game not found' });
     } else {
-      if (!game) {
-        res.status(404).json({ error: 'Game not found' });
-      } else {
-        res.status(204).end();
-      }
+      res.status(204).end();
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
